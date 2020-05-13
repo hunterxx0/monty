f5b19ca4a5e681d8b20162c31027b91b4f0e61c5
medcharfi96 <1389@holbertonschool.com>
2020-05-13 22:43:28 +0200

    add mul dic mod

---
+++ 3func.c
@@ -0,0 +1,102 @@
+#include "monty.h"
+
+/*
+*
+*
+*
+*/
+void div(stack_t **h, unsigned int l)
+{
+	stack_t *tmp = *h;
+	int res = 0;
+
+	if (listsl(*tmp, 1) < 2)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n\n", l);
+		fclose(fd);
+		free_all(file, cmd, *h);
+		exit(EXIT_FAILURE);
+	}
+	if ((*tmp)->n != 0)
+	{
+		res = (((*tmp)->next)->n) / ((*tmp)->n);
+		*h = tmp->next;
+		tmp->next->n = res;
+	}
+	else
+	{
+		fprintf(stderr, "L%u: division by zero\n", l);
+		fclose(fd);
+		free_all(file, cmd, *h);
+		exit(EXIT_FAILURE);
+	}
+}
+/*
+*
+*
+*
+*
+*/
+void fn_mul(stack_t **h, unsigned int l)
+{
+	stack_t *tmp = *h;
+	int val1 = 0; val2 = 0; res = 0;
+
+	if (listsl(*tmp, 1) < 2)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", l);
+		fclose(fd);
+		free_all(file, cmd, *h);
+		exit(EXIT_FAILURE);
+
+	}
+	else
+	{
+	val1 = h->n;
+	val2 = h->next->n;
+	res = val2 * val1;
+	tmp = tmp->next;
+	tmp->n = res;
+	*h = tmp;
+	free(tmp);
+	}
+}
+
+/*
+*
+*
+*
+*
+*/
+void mod (stack_t **h, unsigned int l)
+{
+	stack_t *tmp = *h;
+	int val1 = 0; val2 = 0; res = 0;
+
+	if (listsl(*tmp, 1) < 2)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", l);
+		fclose(fd);
+		free_all(file, cmd, *h);
+		exit(EXIT_FAILURE);
+	}
+	if(tmp->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", l);
+		fclose(fd);
+		free_all(file, cmd, *h);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		val1 = tmp->next->n;
+		val2 = tmp->n;
+		res = val2 % val1;
+		tmp = tmp->next;
+		tmp->n = res;
+		h = tmp;
+		free(tmp);
+
+	}
+
+}
